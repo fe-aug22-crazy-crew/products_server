b961fcd729e1ededcf65679948412d31d5865b94
Andrew Skoryk <andrewtimelessone@gmail.com>
2022-11-30 14:56:28 +0200

    fix: getOne

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(cors());
 
+app.use('/.netlify/functions/server', router);
+
 router.get('/', (req, res) => {
   res.json({
     'fe-aug22-crazy-crew': 'Hello world!',
@@ -17,14 +19,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/phones', phoneController.getAll);
-router.get('/:phoneId', phoneController.getOne);
-router.delete('/:phoneId', phoneController.remove);
+router.get('/phones/:phoneId', phoneController.getOne);
+router.delete('phones/:phoneId', phoneController.remove);
 
 router.get('/categories', categoryController.getAll);
-router.get('/:categoryId', categoryController.getOne);
-
-app.use('/.netlify/functions/server', router);
-
-app.use('/.netlify/functions/server', router);
+router.get('/categories/:categoryId', categoryController.getOne);
 
 export const handler = serverless(app);
